48d33f6f618e44c1c7982f93bd08f0198030c1d8
Jano-Ch <janochiambretto@gmail.com>
2024-11-04 19:39:34 -0300

    maquetado inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ClinicaInfoComponent } from './pages/clinica-info/clinica-info.component';
 import { CemInfoComponent } from './pages/cem-info/cem-info.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     {path: 'home', component: HomeComponent},
     {path: 'clinica', component: ClinicaInfoComponent},
-    {path: 'cem', component: CemInfoComponent}
+    {path: 'cem', component: CemInfoComponent},
+    {path: 'profile', component: ProfileComponent},
+    {path: 'login', component: LoginComponent},
 ];
