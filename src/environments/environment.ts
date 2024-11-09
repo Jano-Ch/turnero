12702c0beedb27b7681db3a81beb156858801377
FranCasasH <franciscocasasher@gmail.com>
2024-11-09 05:44:40 -0300

    firebase conections

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyB3KTxYarpyN106t5-aEeR8T7lN4IuzHXA",
+        authDomain: "turnero-d8808.firebaseapp.com",
+        projectId: "turnero-d8808",
+        storageBucket: "turnero-d8808.firebasestorage.app",
+        messagingSenderId: "433904031433",
+        appId: "1:433904031433:web:d746063fa2cba51f04df18",
+        measurementId: "G-9LJ4YLXYN4"
+    }
+  };
